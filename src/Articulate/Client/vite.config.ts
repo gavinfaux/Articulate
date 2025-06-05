cd255ab8fa202e4a1102b15c0e75212517dd6872
Gavin Faux <gavin.faux@reddev.co.uk>
2025-06-05 11:52:03 +0100

    wip

--- src/Articulate/Client/vite.config.ts
+++ src/Articulate/Client/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       formats: ["es"],
       fileName: (format, entryName) => `${entryName}.js`,
     },
-    outDir: "../wwwroot/App_Plugins/Articulate/BackOffice/",
+    outDir: "../App_Plugins/Articulate/BackOffice/",
     emptyOutDir: true,
     sourcemap: true,
     rollupOptions: {
