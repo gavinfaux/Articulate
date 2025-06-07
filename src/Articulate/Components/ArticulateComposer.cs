7524b5cde4f3598a68a2ab681023a9209d344930
Gavin Faux <gavin.faux@reddev.co.uk>
2025-06-07 21:52:56 +0100

    Remove ServerVariablesParsingHandler
    No longer needed as OpenAPI is now used for API endpoints,  this was used by legacy AngularJS code to build URLs for API endpoints

--- src/Articulate/Components/ArticulateComposer.cs
+++ src/Articulate/Components/ArticulateComposer.cs
@@ -51,7 +51,6 @@ namespace Articulate.Components
             builder.AddNotificationHandler<ContentSavingNotification, ContentSavingHandler>();
             builder.AddNotificationHandler<ContentSavedNotification, ContentSavedHandler>();
             // builder.AddNotificationHandler<ContentTypeSavingNotification, ContentTypeSavingHandler>();
-            //builder.AddNotificationHandler<ServerVariablesParsingNotification, ServerVariablesParsingHandler>();
             builder.AddNotificationHandler<ContentCacheRefresherNotification, ContentCacheRefresherHandler>();
             builder.AddNotificationHandler<DomainCacheRefresherNotification, DomainCacheRefresherHandler>();
 
