c1a78eb835fe02289b3c827df0a2367da1e7ae5a
Gavin Faux <gavin.faux@reddev.co.uk>
2025-06-12 21:29:49 +0100

    ensure uri returns value with relative paths

--- src/Articulate/Syndication/RssFeedGenerator.cs
+++ src/Articulate/Syndication/RssFeedGenerator.cs
@@ -119,6 +119,7 @@ namespace Articulate.Syndication
                 logoUri = rootPageModel.BlogLogo.IsNullOrWhiteSpace()
                     ? null
                     : new Uri(rootPageModel.BlogLogo);
+                    : new Uri(rootPageModel.BlogLogo, UriKind.RelativeOrAbsolute);
             }
             catch (Exception ex)
             {
