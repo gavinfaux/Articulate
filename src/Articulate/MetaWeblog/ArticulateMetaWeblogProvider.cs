43240f247bb1ca7d3546fc23521d9cbb709d1f58
Gavin Faux <gavin.faux@reddev.co.uk>
2025-06-12 21:29:18 +0100

    amend to use MarkdownHelper

--- src/Articulate/MetaWeblog/ArticulateMetaWeblogProvider.cs
+++ src/Articulate/MetaWeblog/ArticulateMetaWeblogProvider.cs
@@ -5,11 +5,9 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Articulate.Models;
-using HeyRed.MarkdownSharp;
 using Newtonsoft.Json;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.IO;
-using Umbraco.Cms.Core.Media;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.Membership;
 using Umbraco.Cms.Core.Models.PublishedContent;
@@ -464,7 +462,7 @@ namespace Articulate.MetaWeblog
 
             description = post.ContentType.Alias == "ArticulateRichText"
             ? post.GetValue<string>("richText")
-            : new Markdown().Transform(post.GetValue<string>("markdown")),
+            : MarkdownHelper.ToHtml(post.GetValue<string>("markdown")),
 
             permalink = post.GetValue<string>("umbracoUrlName").IsNullOrWhiteSpace()
             ? post.Name.ToUrlSegment(_shortStringHelper)
