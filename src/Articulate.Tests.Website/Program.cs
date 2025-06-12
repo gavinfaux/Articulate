e89b52a9c05ee0a2fceb700f6042fb9d147fb5e8
Gavin Faux <gavin.faux@reddev.co.uk>
2025-06-12 22:35:52 +0100

    Update test website
      - switch to minimal hosting model from generic host
      - remove legacy grid
      - add block grid and defaults from Umbraco 15 installer
      - remove debug page and razor pages support

--- src/Articulate.Tests.Website/Program.cs
+++ src/Articulate.Tests.Website/Program.cs
@@ -1,24 +1,81 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Smidge;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Extensions;
 
-namespace Articulate.Tests.Website
+var builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddComposers()
+    .Build();
+
+builder.Services.AddSmidge(builder.Configuration.GetSection("smidge"));
+
+builder.Services.Configure<IISServerOptions>(options => options.MaxRequestBodySize = int.MaxValue);
+
+builder.Services.Configure<KestrelServerOptions>(options => options.Limits.MaxRequestBodySize = int.MaxValue);
+
+builder.Services.Configure<FormOptions>(x =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-            => CreateHostBuilder(args)
-                .Build()
-                .Run();
+    x.ValueLengthLimit = int.MaxValue;
+    x.MultipartBodyLengthLimit = int.MaxValue;
+    x.MultipartHeadersLengthLimit = int.MaxValue;
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureUmbracoDefaults()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStaticWebAssets();
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+if (app.Environment.IsDevelopment())
+{
+    _ = app.UseDeveloperExceptionPage();
 }
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        _ = u.UseBackOffice();
+        _ = u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            _ = u.EndpointRouteBuilder.MapGet("/debug/routes", (IEnumerable<EndpointDataSource> endpointSources) =>
+            {
+                var endpoints = endpointSources
+                    .SelectMany(es => es.Endpoints)
+                    .OfType<RouteEndpoint>();
+
+                var output = endpoints.Select(e => new
+                {
+                    Priority = e.Order,
+                    Name = e.DisplayName,
+                    Route = e.RoutePattern.RawText,
+                    Method = e.Metadata.OfType<HttpMethodMetadata>().FirstOrDefault()?.HttpMethods.FirstOrDefault()
+                })
+                .OrderBy(e => e.Priority)
+                .ToList();
+
+                return Results.Ok(output);
+            });
+        }
+
+        _ = u.UseUmbracoPreviewEndpoints();
+        _ = u.UseBackOfficeEndpoints();
+        _ = u.UseWebsiteEndpoints();
+    });
+
+app.UseSmidge();
+
+app.Run();
