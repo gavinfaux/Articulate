146590eaf1d01f7ad9d41779a2c3bd0a0fa7ce71
Gavin Faux <gavin.faux@reddev.co.uk>
2025-08-11 05:56:11 +0100

    QA
    Google Tags

--- src/Articulate/Extensions/PublishedContentExtensions.cs
+++ src/Articulate/Extensions/PublishedContentExtensions.cs
@@ -322,32 +322,36 @@ namespace Articulate.Extensions
 
         public static IHtmlContent GoogleAnalyticsTracking(this IMasterModel model)
         {
-            /*
-             * <!-- Google tag (gtag.js) -->
-               <script async src="https://www.googletagmanager.com/gtag/js?id=G-VMZC8VN8PN"></script>
-               <script>
-                 window.dataLayer = window.dataLayer || [];
-                 function gtag(){dataLayer.push(arguments);}
-                 gtag('js', new Date());
-
-                 gtag('config', 'G-VMZC8VN8PN');
-               </script>
-             */
-            if (model.RootBlogNode.Value<string>("googleAnalyticsId").IsNullOrWhiteSpace() == false
-                && model.RootBlogNode.Value<string>("googleAnalyticsName").IsNullOrWhiteSpace() == false)
+            var tag = model.RootBlogNode.Value<string>("googleAnalyticsId") ?? string.Empty;
+            if (tag.IsNullOrWhiteSpace() == false)
             {
-                return new HtmlString("""
-                                      <script>
-                                        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                                        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                                        ga('create', '
-                                      """ + model.RootBlogNode.Value<string>("googleAnalyticsId") + @"', '" + model.RootBlogNode.Value<string>("googleAnalyticsName") + """
-                    ');
-                      ga('send', 'pageview');
-                    </script>
-                    """);
+                return new HtmlString(
+                    $$"""
+                      <!-- Google Tag Manager -->
+                      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','{{tag}}');</script>
+                      <!-- End Google Tag Manager -->
+                      """);
+            }
+
+            return new HtmlString(string.Empty);
+        }
+
+        public static IHtmlContent GoogleAnalyticsNoScript(this IMasterModel model)
+        {
+            var tag = model.RootBlogNode.Value<string>("googleAnalyticsId") ?? string.Empty;
+            if (tag.IsNullOrWhiteSpace() == false)
+            {
+                return new HtmlString(
+                    $"""
+                     <!-- Google Tag Manager (noscript) -->
+                     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={tag}"
+                     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+                     <!-- End Google Tag Manager (noscript) -->
+                     """);
             }
 
             return new HtmlString(string.Empty);
