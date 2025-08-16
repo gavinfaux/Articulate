c67118c655b4860bc02a16e9082a575be6b396dc
Gavin Faux <gavin.faux@reddev.co.uk>
2025-08-16 03:17:05 +0100

    Static Bundles for Themes

--- src/Articulate.Web/wwwroot/App_Plugins/Articulate/Themes/VAPOR/assets/js/a11y.js
+++ src/Articulate.Web/wwwroot/App_Plugins/Articulate/Themes/VAPOR/assets/js/a11y.js
@@ -2,15 +2,15 @@
 document.querySelector('.skip-link').addEventListener('click', function(e) {
     // Get the href value (like '#main-content') and find that element
     const target = document.querySelector(this.getAttribute('href'));
-    
+
     // If the target element exists
     if (target) {
       // Prevent the default anchor link behavior
       e.preventDefault();
-      
+
       // Make the target focusable (if it isn't already)
       target.setAttribute('tabindex', '-1');
-      
+
       // Move focus to the target element
       target.focus();
     }
