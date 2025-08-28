696d419e324fc57ea4f7759b39dedd24513ea51d
Gavin Faux <gavin.faux@reddev.co.uk>
2025-08-28 11:50:41 +0100

    nope

--- src/Articulate/Migrations/Upgrade/ArticulatePlan.cs
+++ src/Articulate/Migrations/Upgrade/ArticulatePlan.cs
@@ -20,15 +20,8 @@ public sealed class ArticulatePlan : MigrationPlan
         : base(ArticulateConstants.Migration.ArticulatePackageMigrationPlan)
         => DefinePlan(umbracoVersion);
 
-    /// <inheritdoc />
-    public override string InitialState => string.Empty;
-
     private void DefinePlan(IUmbracoVersion umbracoVersion)
     {
-        From(InitialState);
-
-        To<Articulate.Migrations.Upgrade.V_6_0_0.MigrateImageCropperToJson>("{A26A8851-CB9F-4E02-9DCF-1937DEF8ABE1}");
-
         if (umbracoVersion.SemanticVersion.Major >= 16)
         {
             To<Articulate.Migrations.Upgrade.V_6_0_0.MigrateV16DataTypeConfiguration>("{5B6B5B4C-F79A-4CC7-9D77-5F0326BD94FE}");
