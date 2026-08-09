f829a3169994a4c6203b8794849bd02c4c8d0763
Gavin Faux <gavin.faux@reddev.co.uk>
2026-08-09 23:11:16 +0100

    fix(metaweblog): harden XML request parsing

--- src/Articulate.Tests/Controllers/MetaWeblogControllerTests.cs
+++ src/Articulate.Tests/Controllers/MetaWeblogControllerTests.cs
@@ -8,7 +8,7 @@ namespace Articulate.Tests.Controllers
     public class MetaWeblogControllerTests
     {
         [Test]
-        public void IsValidXmlRpcEnvelope_returns_true_for_valid_method_call()
+        public void TryNormalizeMetaWeblogRequest_returns_true_for_valid_method_call()
         {
             const string content = """
                 <?xml version="1.0"?>
@@ -18,11 +18,11 @@ namespace Articulate.Tests.Controllers
                 </methodCall>
                 """;
 
-            Assert.That(MetaWeblogController.IsValidXmlRpcEnvelope(content), Is.True);
+            Assert.That(MetaWeblogController.TryNormalizeMetaWeblogRequest(content, 1000, out _), Is.True);
         }
 
         [Test]
-        public void IsValidXmlRpcEnvelope_returns_false_when_root_is_not_methodCall()
+        public void TryNormalizeMetaWeblogRequest_returns_false_when_root_is_not_methodCall()
         {
             const string content = """
                 <?xml version="1.0"?>
@@ -31,11 +31,11 @@ namespace Articulate.Tests.Controllers
                 </methodResponse>
                 """;
 
-            Assert.That(MetaWeblogController.IsValidXmlRpcEnvelope(content), Is.False);
+            Assert.That(MetaWeblogController.TryNormalizeMetaWeblogRequest(content, 1000, out _), Is.False);
         }
 
         [Test]
-        public void IsValidXmlRpcEnvelope_returns_false_when_methodName_element_is_absent()
+        public void TryNormalizeMetaWeblogRequest_returns_false_when_methodName_element_is_absent()
         {
             const string content = """
                 <?xml version="1.0"?>
@@ -44,27 +44,43 @@ namespace Articulate.Tests.Controllers
                 </methodCall>
                 """;
 
-            Assert.That(MetaWeblogController.IsValidXmlRpcEnvelope(content), Is.False);
+            Assert.That(MetaWeblogController.TryNormalizeMetaWeblogRequest(content, 1000, out _), Is.False);
         }
 
         [Test]
-        public void IsValidXmlRpcEnvelope_returns_false_for_malformed_xml()
+        public void TryNormalizeMetaWeblogRequest_returns_false_for_malformed_xml()
         {
             const string content = "<methodCall><methodName>foo</not-closed>";
 
-            Assert.That(MetaWeblogController.IsValidXmlRpcEnvelope(content), Is.False);
+            Assert.That(MetaWeblogController.TryNormalizeMetaWeblogRequest(content, 1000, out _), Is.False);
         }
 
         [Test]
-        public void IsValidXmlRpcEnvelope_returns_false_for_empty_string()
+        public void TryNormalizeMetaWeblogRequest_returns_false_for_empty_string()
         {
-            Assert.That(MetaWeblogController.IsValidXmlRpcEnvelope(string.Empty), Is.False);
+            Assert.That(MetaWeblogController.TryNormalizeMetaWeblogRequest(string.Empty, 1000, out _), Is.False);
         }
 
         [Test]
-        public void IsValidXmlRpcEnvelope_returns_false_for_plain_text()
+        public void TryNormalizeMetaWeblogRequest_returns_false_for_plain_text()
         {
-            Assert.That(MetaWeblogController.IsValidXmlRpcEnvelope("not xml at all"), Is.False);
+            Assert.That(MetaWeblogController.TryNormalizeMetaWeblogRequest("not xml at all", 1000, out _), Is.False);
+        }
+
+        [Test]
+        public void TryNormalizeMetaWeblogRequest_rejects_doctype()
+        {
+            const string content = "<!DOCTYPE methodCall [<!ENTITY x 'expanded'>]><methodCall><methodName>&x;</methodName></methodCall>";
+
+            Assert.That(MetaWeblogController.TryNormalizeMetaWeblogRequest(content, 1000, out _), Is.False);
+        }
+
+        [Test]
+        public void TryNormalizeMetaWeblogRequest_rejects_character_limit()
+        {
+            const string content = "<methodCall><methodName>blogger.getUsersBlogs</methodName></methodCall>";
+
+            Assert.That(MetaWeblogController.TryNormalizeMetaWeblogRequest(content, 10, out _), Is.False);
         }
     }
 }
