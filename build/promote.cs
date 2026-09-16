d38fb10c6cdb1a5a67b9384432f3bd8ce1f25200
Gavin Faux <gavin.faux@reddev.co.uk>
2026-09-16 19:14:07 +0100

    fix(build): enable reflection JSON serializer for promotion manifest

--- build/promote.cs
+++ build/promote.cs
@@ -1,5 +1,6 @@
 #!/usr/bin/env -S dotnet --
 #:property NoWarn=SA1400,SA1503,SA1519,SA1116,SA1117,SA1122,SA1649,IDE0008,IDE0011,IDE0040,SA1500,IL2026,IL3050
+#:property JsonSerializerIsReflectionEnabledByDefault=true
 
 #nullable enable
 
